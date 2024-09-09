9ca994c0d2a0da77d3abc864a080d43ee52a8210
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-09-09 16:02:27 -0300

    Add function to get the computer choice

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    switch(randomNumber) {
+        case 0: 
+            return 'Rock';
+        case 1: 
+            return 'Paper';
+        case 2: 
+            return 'Scissors';
+    }
+}
