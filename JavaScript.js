e1af152dc92256669ee1aa5b4d71163d89236402
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-09-16 11:04:54 -0300

    Add logic function for rounds

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -14,13 +17,42 @@ function getComputerChoice() {
 function getHumanChoice() {
     let choice = prompt('Choose your weapon: Rock, Paper, Scissors!');
 
-    if (choice.toUpperCase() === 'ROCK') {
-        return 'You chose Rock!';
-    } else if (choice.toUpperCase() === 'PAPER') {
-        return 'You chose Paper!';
-    } else if (choice.toUpperCase() === 'SCISSORS') {
-        return 'You chose Scissors!';
+    if (choice.toLowerCase() === 'rock') {
+        return 'Rock';
+    } else if (choice.toLowerCase() === 'paper') {
+        return 'Paper';
+    } else if (choice.toLowerCase() === 'scissors') {
+        return 'Scissors';
     } else {
-        return 'That\'s not a valid choice!';
+        return 'invalid';
     }
 }
+
+function playRound(getHumanChoice, getComputerChoice){
+
+    let human = getHumanChoice.toLowerCase();
+    let computer = getComputerChoice.toLowerCase();
+
+    if (human === 'invalid') {
+        console.log('That\'s not a valid choice!'); return;
+    }
+
+    if (human === computer) {
+        console.log('It\'s a tie! You both chose ' + getHumanChoice);
+    } else if (
+        (human === 'rock' && computer === 'scissors') ||
+        (human === 'paper' && computer === 'rock') ||
+        (human === 'scissors' && computer === 'paper')
+    ) {
+        console.log('You win! ' + getHumanChoice + ' beats ' + getComputerChoice);
+        ++humanScore;
+    } else {
+        console.log('You lose! ' + getComputerChoice + ' beats ' + getHumanChoice + ', try again!');
+        ++computerScore;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
