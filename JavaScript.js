ec15bd093a3196e59bb2e7dc7e580f46609abeec
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-09-16 12:54:40 -0300

    Modify console.log for alert message to display on screen

--- JavaScript.js
+++ JavaScript.js
@@ -36,27 +36,27 @@ function playRound(getHumanChoice, getComputerChoice){
     let computer = getComputerChoice.toLowerCase();
 
     if (human === 'invalid') {
-        console.log('That\'s not a valid choice!'); return;
+        alert('That\'s not a valid choice!'); return;
     }
 
     if (human === computer) {
-        console.log('You both chose ' + getHumanChoice);
+        alert('You both chose ' + getHumanChoice);
     } else if (
         (human === 'rock' && computer === 'scissors') ||
         (human === 'paper' && computer === 'rock') ||
         (human === 'scissors' && computer === 'paper')
     ) {
-        console.log('You win this round! ' + getHumanChoice + ' beats ' + getComputerChoice);
+        alert('You win this round! ' + getHumanChoice + ' beats ' + getComputerChoice);
         humanScore++;
     } else {
-        console.log('You lose this round! ' + getComputerChoice + ' beats ' + getHumanChoice + ', try again!');
+        alert('You lose this round! ' + getComputerChoice + ' beats ' + getHumanChoice + ', try again!');
         computerScore++;
     }
 }
 
 function playGame() {
 playRound(getHumanChoice(), getComputerChoice());
-console.log('You: ' + humanScore + ' Computer: ' + computerScore);
+alert('You: ' + humanScore + ' Computer: ' + computerScore);
 }
 
 while (humanScore !== 5 && computerScore !== 5) {
@@ -64,9 +64,9 @@ while (humanScore !== 5 && computerScore !== 5) {
     }
 
 if (humanScore === 5) {
-    console.log('You win the game! Are you some kind of seer?');
+    alert('You win the game! Are you some kind of seer?');
 } else if (humanScore === computerScore) {
-    console.log('It\'s a tie... That\'s unacceptable, please play again!');
+    alert('It\'s a tie... That\'s unacceptable, please play again!');
 } else {
-    console.log('You lose the game! With practice the student becomes the master');
+    alert('You lose the game! With practice the student becomes the master');
 }
