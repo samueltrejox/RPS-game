e92793cbc5c79fdee61f3414d09ec2d52b201808
samueltrejox <101751524+samueltrejox@users.noreply.github.com>
2024-10-16 14:45:28 -0300

    Fix game logic to stop at 5 points and Add reset button

--- JavaScript.js
+++ JavaScript.js
@@ -2,13 +2,16 @@ let playerScore = 0;
 let computerScore = 0;
 
 const btns = document.querySelectorAll(".btns");
+const section2 = document.querySelector("#section-2");
 const results = document.querySelector("#results");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const playerSelection = btn.id;
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    if (playerScore < 5 && computerScore < 5) {
+      const playerSelection = btn.id;
+      const computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection);
+    }
   });
 });
 
@@ -25,9 +28,8 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
+    results.innerText = playerSelection + " beats " + computerSelection + ", you win this round!";
     playerScore++;
-    results.innerText =
-      playerSelection + " beats " + computerSelection + ", you win this round!";
   } else {
     computerScore++;
     results.innerText =
@@ -35,16 +37,58 @@ function playRound(playerSelection, computerSelection) {
       " beats " +
       playerSelection +
       ", better luck next time D:";
+    }
+
+    updateScore();
+    
+    if (playerScore === 5 || computerScore === 5) {
+      gameOver();
+    }
+}
+
+function gameOver() {
+  results.innerText = '';
+  const winnerMessage = document.createElement("div");
+  winnerMessage.classList.add("small-br");
+  
+  if (playerScore === 5) {
+    winnerMessage.innerText = "Congratulations, you win! You deceived the computer";
+  } else if (computerScore === 5) {
+    winnerMessage.innerText = "You lose! With practice the student becomes the master";
   }
-  results.innerText += `\nPlayer Score: ${playerScore}, Computer Score: ${computerScore}`;
+
+  section2.appendChild(winnerMessage);
+  restartBtn();
 }
 
-/* while (humanScore != 5 && computerScore != 5) {
-    playGame();
+function restartBtn() {
+  const restartBtn = document.createElement("button");
+  restartBtn.classList.add("restart");
+  restartBtn.innerText = "Restart";
+
+  restartBtn.addEventListener("click", () => {
+    restartGame();
+  });
+  section2.appendChild(restartBtn);
 }
 
-if (humanScore === 5) {
-    console.log('You win the game! Are you some kind of seer?');
-} else if (computerScore === 5) {
-    console.log('You lose the game! With practice the student becomes the master');
-} */
+function updateScore() {
+  const score = document.querySelector("#score");
+  score.innerText = `Player Score: ${playerScore}, Computer Score: ${computerScore}`;
+}
+
+function restartGame() {
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+
+  const existingWinner = section2.querySelector(".small-br");
+  if (existingWinner) {
+    existingWinner.remove();
+  }
+
+  const existingRestartBtn = section2.querySelector(".restart");
+  if (existingRestartBtn) {
+    existingRestartBtn.remove();
+  }
+}
